f25e1c0e2fc376c5fd637098efe69e3ec5a3adab
Abedela Bedru <a2dul.12a@gmail.com>
2026-03-24 12:34:22 +0300

    folder structure changes of test files

--- src/crawler/queue.ts
+++ src/crawler/queue.ts
@@ -15,13 +15,13 @@ export class Queue {
 
   // Appending to the end of the queue
   enqueue(link: string): void {
-    const newLink = new DoublyNode(link)
+    const newLink = new DoublyNode(link);
 
     if (!this.head) {
       this.head = this.tail = newLink;
       this.increaseSize();
       return;
-    }
+    };
 
     if (this.tail) {
       this.tail.next = newLink;
@@ -29,15 +29,16 @@ export class Queue {
       this.tail = newLink;
       this.increaseSize();
       return;
-    }
-  }
+    };
+  };
 
   // Shifting / Removing the first element in the queue
   dequeue() {
     if (this.head === this.tail) {
       this.head = this.tail = null;
+      this.decreaseSize();
       return;
-    }
+    };
 
     if (this.head) {
       const headNext = this.head.next;
@@ -46,29 +47,29 @@ export class Queue {
       this.head.prev = null;
       this.decreaseSize();
       return
-    }
-  }
+    };
+  };
 
   isEmpty(): number {
     if (!this.head || !this.tail) {
-      return 1;
+      return true;
     } else {
-      return 0;
-    }
-  }
+      return false;
+    };
+  };
 
   increaseSize(): void{
     this.size += 1;
     return;
-  }
+  };
 
   decreaseSize(): void{
     this.size -= 1;
     return;
-  }
+  };
 
   size(): number {
     return this.size;
-  }
+  };
 };
 
--- tests/crawler/queue.test.ts
+++ tests/crawler/queue.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, test } from 'vitest';
-import {Queue} from "./queue.ts";
+import {Queue} from "../../src/crawler/queue.ts";
 
 const objs = new Queue<string>();
 
@@ -8,22 +8,22 @@ describe('queue', () => {
     objs.enqueue("check 1");
     objs.enqueue("check 2");
 
-    expect(objs.size).toBe(2);
+    expect(objs.size).toEqual(2);
   });
 
   test('Dequeue', () => {
     objs.dequeue();
     objs.dequeue();
-
-    expect(objs.size).toBe(1);
+    
+    expect(objs.size).toEqual(0);
   });
 
   test('Mentioning Size', () => {
-    expect(objs.size).toBe(0);
+    expect(objs.size).toEqual(0);
   });
 
   test('Confirming Element availability', () => {
-    expect(objs.isEmpty()).toBe(0);
+    expect(objs.isEmpty()).toBe(true);
   });
 });
 
