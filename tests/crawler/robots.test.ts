04fbcc20233f0cc331a7cf4eb3d74952a8112674
Abedela Bedru <a2dul.12a@gmail.com>
2026-03-30 20:49:04 +0300

    fixes: ts rules and unnecessary mocking
    - Implemented Typescript rules in module importing (.js)
    - Removed Mock tests as it defeats the purpose of testing

--- tests/crawler/robots.test.ts
+++ tests/crawler/robots.test.ts
@@ -1,21 +1,8 @@
-import { describe, test, expect, vi, beforeEach } from "vitest";
-import { RobotsTxt } from "../../src/crawler/robots";
+import { describe, test, expect } from "vitest";
+import { RobotsTxt } from "../../src/crawler/robots.js";
 
 describe("RobotsTxt", () => {
-  beforeEach(() => {
-    vi.resetAllMocks();
-  });
-
   test("blocks disallowed path", async () => {
-    // mock robots.txt
-    global.fetch = vi.fn().mockResolvedValue({
-      ok: true,
-      text: async () => `
-        User-agent: *
-        Disallow: /admin
-      `,
-    } as any);
-
     const robots = new RobotsTxt();
 
     const res = await robots.isAllowed("https://example.com/admin");
@@ -23,50 +10,21 @@ describe("RobotsTxt", () => {
     expect(res).toBe(false);
   });
 
-  test("allows non-disallowed path", async () => {
-    global.fetch = vi.fn().mockResolvedValue({
-      ok: true,
-      text: async () => `
-        User-agent: *
-        Disallow: /admin
-      `,
-    } as any);
-
+  test("accepts allowed path", async () => {
     const robots = new RobotsTxt();
 
-    const res = await robots.isAllowed("https://example.com/home");
+    const res = await robots.isAllowed(
+      "https://en.wikipedia.org/wiki/Computer_science",
+    );
 
     expect(res).toBe(true);
   });
 
   test("allows everything if robots.txt missing", async () => {
-    global.fetch = vi.fn().mockResolvedValue({
-      ok: false,
-    } as any);
-
     const robots = new RobotsTxt();
 
     const res = await robots.isAllowed("https://example.com/admin");
 
     expect(res).toBe(true);
-  });
-
-  test("uses cache (fetch called once)", async () => {
-    const fetchMock = vi.fn().mockResolvedValue({
-      ok: true,
-      text: async () => `
-        User-agent: *
-        Disallow: /admin
-      `,
-    } as any);
-
-    global.fetch = fetchMock;
-
-    const robots = new RobotsTxt();
-
-    await robots.isAllowed("https://example.com/admin");
-    await robots.isAllowed("https://example.com/home");
-
-    expect(fetchMock).toHaveBeenCalledTimes(1);
   });
 });
